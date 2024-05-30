d49df64ef6c4467530cd3d4d3b401a3ec3672ca3
mark <markbmicheal419@gmail.com>
2024-05-30 12:45:59 +0100

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE !'));
+app.use(express.static('PUBLIC'));
+
+//app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE CREATED WITH AWS THIS PACE INDICATES SUCCESS !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
